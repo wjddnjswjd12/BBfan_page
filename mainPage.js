efe5bd6a35933a9c36cf1ef704dcf47684058f65
Jungwon Lee <leejungwon96@gmail.com>
2020-12-31 11:31:32 +0900

    applied IntersectionObserver to change the background color of header when scrolled down

--- mainPage.js
+++ mainPage.js
@@ -1,5 +1,5 @@
-const header = document.querySelector(".header");
-const sectionOne = document.querySelector(".home_title_top");
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".section_home");
 
 const sectionOneOptions = {
   rootMargin: "-200px 0px 0px 0px",
